63be2851d319bd9b492cf0c86a2758824271862f
Santosh Venkatraman <santosh.venk@gmail.com>
2017-03-20 21:25:00 +0530

    Fix styles

--- index.android.js
+++ index.android.js
@@ -90,13 +90,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  button: {
+    padding: 10
   },
   todo: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: 'center'
   }
 });
 
