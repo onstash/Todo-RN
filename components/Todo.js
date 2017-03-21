a31b80ad9414ff9e6163803d5afebf48ec381073
Santosh Venkatraman <santosh.venk@gmail.com>
2017-03-21 12:55:40 +0530

    Fix project structure (#5)

---
+++ components/Todo.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from 'react-native';
+
+
+export default class Todo extends Component {
+  toggleTodo(data) {
+    data.done = !data.done;
+    if (data.done) {
+      data.completedAt = new Date();
+    }
+    const updatedTodos = [];
+    this.state.todos.map(todo => {
+      if (todo.id == data.id) {
+        updatedTodos.push(data);
+      } else {
+        updatedTodos.push(todo);
+      }
+    });
+    this.setState({ todos: updatedTodos });
+  }
+
+  deleteTodo(data) {
+    const updatedTodos = [];
+    this.state.todos.map(todo => {
+      if (todo.id !== data.id) {
+        updatedTodos.push(todo);
+      }
+    });
+    this.setState({ todos: updatedTodos });
+  }
+
+  addToDo() {
+    const { text, todos } = this.state;
+    if (!text) {
+      return;
+    }
+    const todo = {
+      task: text,
+      id: this.state.todos.length + 1,
+      createdAt: new Date()
+    };
+    const updatedTodos = [].concat(todos, [todo]);
+    this.setState({ todos: updatedTodos, text: '' });
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      todos: [
+        {
+          task: 'Learn React Native', createdAt: new Date(), id: 0
+        },
+        {
+          task: 'Build Todo app', createdAt: new Date(), id: 1
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to my Todo app!
+        </Text>
+        <TextInput
+          placeholder="Type here to add todo"
+          onChangeText={ text => this.setState({ text })}>
+          { this.state.text }
+        </TextInput>
+        <Button
+          onPress={ () => this.addToDo() }
+          title="Add"
+        />
+        {
+          this.state.todos.map((todo, index) => {
+            return (
+              <Text
+                key={ index }
+                style={ styles.todo }
+                onPress={ () => this.toggleTodo(todo) }
+                onLongPress={ () => this.deleteTodo(todo) }>
+                { todo.task } ({ todo.done ? 'Finished': 'Not finished' })
+              </Text>
+            );
+          })
+        }
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  button: {
+    padding: 10
+  },
+  todo: {
+    fontSize: 20,
+    textAlign: 'center'
+  }
+});
